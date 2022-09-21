9eb50ef704b31cb4648bf5205e0e5eba82859c62
KjetilMainPc <kjetilindrehus@gmail.com>
2022-09-21 21:39:24 +0200

    Minor changes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,7 +10,7 @@ export const About = () => {
             </div>
 
             <div>
-                <div className="bg-main rounded-xl mx-10 border-2 p-4 m-8 text-jost text-lg md:p-6 md:text-xl md:w-1/2 drop-shadow-[0_35px_35px_rgba(0,0,0,0.3)] ">
+                <div className="bg-main rounded-xl mx-10 border-2 p-4 m-8 text-jost text-sm md:p-6 md:text-xl md:w-1/2 drop-shadow-[0_35px_35px_rgba(0,0,0,0.3)] ">
                     <p className="">
                         I started coding in High School with <span className="underline decoration-red decoration-4 underline-offset-8 ">Python</span>. The challege was the primary reason. My first code project was with python and challeging.
                         I created a game with using PyGame. It was terrible, but I spend a whole weekend on it. Later on I wanted more challege, so I stared learning 

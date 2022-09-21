2d421e94e8d381a3d02fd4d9fd84677c7b5116a4
KjetilMainPc <kjetilindrehus@gmail.com>
2022-09-21 18:24:13 +0200

    DEL: Delete last version

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import './App.css';
-import Hero from './components/Hero';
-import Navbar from './components/Navbar';
-import MySkills from './components/MySkills';
-import About from './components/About';
-import Contact from './components/Contact';
+
 
 function App() {
   return (
     <div className="bg-sec  h-full">
-      <Navbar/>
-      <Hero/>
-      <MySkills/>
-      <About/>
+      Test
     </div>
   );
 }

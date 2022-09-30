0716618ce106bcc2b533403dc575907e7e84a041
KjetilMainPc <kjetilindrehus@gmail.com>
2022-09-30 15:40:56 +0200

    fix: text in about-section is centered

--- src/components/About/Title.jsx
+++ src/components/About/Title.jsx
@@ -2,8 +2,15 @@ import React from "react";
 
 export const Title = (props) => {
     return (
-        <div className="text-center py-6">
-            <h1 className="text-sec font-jost font-bold text-center text-5xl">{props.text}</h1>
+        <div className="flex justify-center py-6">
+            <div className="relative">
+                <h1 className="text-sec font-jost text-5xl font-bold relative z-10 w-full">
+                    {props.text}
+                </h1>
+                <h1 className="text-orangetheme font-jost text-5xl font-bold absolute z-1 left-1 top-1 w-full">
+                    {props.text}
+                </h1>
+            </div>
         </div>
     );
 };

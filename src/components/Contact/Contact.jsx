2a192f67d3db4b7ae5eb8821f46ebd63a5c82d7d
KjetilMainPc <kjetilindrehus@gmail.com>
2022-10-02 18:22:33 +0200

    fix: metadata corrected

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -24,7 +24,6 @@ export const Contact = () => {
                 </ContactList>
             </ul>
 
-            
         </div>
     );
 };
--- src/components/Contact/ContactList.jsx
+++ src/components/Contact/ContactList.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const ContactList = (props) => {
     return (
-        <a href={props.link} className="font-jost cursor-pointer border-spacing border-2 border-sec w-[50%] transform transition duration-500 hover:scale-110 hover:bg-sec hover:text-main" target="_blank">
+        <a href={props.link} rel="noreferrer" className="font-jost cursor-pointer border-spacing border-2 border-sec w-[50%] transform transition duration-500 hover:scale-110 hover:bg-sec hover:text-main" target="_blank">
             <div className="flex gap-3 py-2 text-3xl items-center justify-center ">
                 {props.children}
             </div>

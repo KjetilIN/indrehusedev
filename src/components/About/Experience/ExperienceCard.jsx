e2d7bebaa6c10232134dcfe47c325d46cec97448
KjetilMainPc <kjetilindrehus@gmail.com>
2022-10-02 19:40:23 +0200

    fix: exp icons centered

--- src/components/About/Experience/ExperienceCard.jsx
+++ src/components/About/Experience/ExperienceCard.jsx
@@ -2,20 +2,21 @@ import React from "react";
 
 export const ExperienceCard = (props) => {
     return (
-        <div>  
-            <div className="avatar relative -bottom-7 z-10 left-[8rem] animate-bounce">
+        <div>
+            <div className="flex justify-center">
+                <div className="avatar relative -bottom-7 z-10 animate-bounce">
                     <div className="w-24 rounded-full ring ring-orangetheme ring-offset-base-100 ring-offset-0">
                         <img src={props.img} alt={props.title} />
                     </div>
                 </div>
-            <div className="card w-96 bg-sec text-white">
+            </div>
+
+            <div className="card w-[100%] md:w-96 bg-sec text-white">
                 <div className="card-body">
                     <h2 className="card-title">{props.title}</h2>
                     <p>{props.text}</p>
                 </div>
             </div>
-        
         </div>
-        
     );
 };

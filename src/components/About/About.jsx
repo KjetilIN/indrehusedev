45fe92407c51b8c2f4cb732dcc6f0f5acb054fb7
KjetilMainPc <kjetilindrehus@gmail.com>
2022-10-02 18:37:11 +0200

    refactor: experice as list of objects

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,6 +6,27 @@ import { InfoCard } from "./InfoCard";
 import { ExperienceCard } from "./Experience/ExperienceCard";
 
 export const About = () => {
+    const experienceList = [
+        {
+            title:"Start Gjøvik",
+            text: "IT repsonsible for website",
+            img: "../img/avatar/start.png",
+        },
+        {
+            title:"Bekk Summer Intern 2023",
+            text: "Internship as Developer",
+            img: "../img/avatar/bekk.png",
+        },
+        {
+            title:"NTNU",
+            text: "Computer Engineering student",
+            img: "../img/avatar/ntnu.png",
+        }
+    ]
+
+
+
+
     return (
         <div className="flex-col justify-center h-auto bg-main rounded-xl">
             <Title text="About Me!"/>
@@ -15,9 +36,9 @@ export const About = () => {
             </div>
             <Experience/>
             <div className="flex flex-col flex-wrap items-center gap-5 md:flex-row md:justify-center py-4">
-                <ExperienceCard title="Start Gjøvik" text="IT repsonsible for website" img="../img/avatar/start.png"/>
-                <ExperienceCard title="Bekk Summer Intern 2023" text="Internship as Developer" img="../img/avatar/bekk.png"/>
-                <ExperienceCard title="NTNU" text="Computer Engineering student"img="../img/avatar/ntnu.png" />
+                {experienceList.map((e) => (
+                    <ExperienceCard title={e.title} text={e.text} img={e.img} key={e.title}/>
+                ))}
             </div>
 
         </div>

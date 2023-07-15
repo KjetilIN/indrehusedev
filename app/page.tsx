de36582109487bf955b4c460c020050d939f4d58
Kjetil Indrehus <kjetilindrehus@gmail.com>
2023-07-15 21:39:06 +0200

    fix:mobile view

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,9 @@ export default function Home() {
 
   return (
     <main className="min-w-full bg-gradient-to-br from-gray-900 via-gradient-mid to-gradient-start min-h-screen flex flex-col items-center backdrop-filter backdrop-blur-3xl">
-      <h1 className="font-mono text-6xl mt-80 font-bold">{TITLE}</h1>
-      <div className="flex flex-col text-left gap-2 mt-2">
-        <h2 className="font-bold font-mono text-3xl mt-5 bg-gradient-to-br text-transparent bg-clip-text from-purple-500 to-pink-500">
+      <h1 className="font-mono text-4xl mt-10  font-bold md:text-6xl md:mt-80">{TITLE}</h1>
+      <div className="flex flex-col text-left gap-2 mt-2 px-4 md:px-0">
+        <h2 className="font-bold font-mono text-xl mt-20 bg-gradient-to-br text-transparent bg-clip-text from-purple-500 to-pink-500 md:text-3xl md:mt-5">
           Developer
         </h2>
         <p className="font-mono font-xl">
